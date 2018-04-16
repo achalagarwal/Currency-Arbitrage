f56e304f18930caf39acab4934ffec4e1f7bb359
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-04-16 16:53:21 +0530

    Initial Commit

---
+++ Currency/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Currency
+//
+//  Created by Achal Agarwal on 16/04/18.
+//  Copyright © 2018 Achal Agarwal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
