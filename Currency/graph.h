baa12d432b9aab8d2ed6e6b51c4608a666189c62
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-04-20 13:47:38 +0530

    Header file for currency graph

--- Currency/graph.h
+++ Currency/graph.h
@@ -6,9 +6,34 @@
 //  Copyright © 2018 Achal Agarwal. All rights reserved.
 //
 
+#ifndef hash_h
+#include "hash.h"
+#endif
+
 #ifndef graph_h
 #define graph_h
 
 #include <stdio.h>
 
 #endif /* graph_h */
+
+struct vertex{
+    void* e;
+    int id;
+};
+
+typedef struct vertex* Vertex;
+
+struct graph{
+    HT table;
+    double** adj;
+    Vertex* vertices;
+    int size;
+    int (*hash)(void*,int);
+    int (*compare)(void*,void*);
+};
+
+typedef struct graph* Graph;
+
+
+
