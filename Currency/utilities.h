6ff886f6220b144ba61833da53d5e8773a084649
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-04-22 03:05:04 +0530

    Added modular functions for realtime work

--- Currency/utilities.h
+++ Currency/utilities.h
@@ -5,10 +5,25 @@
 //  Created by Achal Agarwal on 22/04/18.
 //  Copyright © 2018 Achal Agarwal. All rights reserved.
 //
-
+#ifndef graph_h
+#include "graph.h"
+#endif
+#ifndef hash_h
+#include "hash.h"
+#endif
 #ifndef utilities_h
 #define utilities_h
 
 #include <stdio.h>
 
 #endif /* utilities_h */
+
+struct array{
+    int size;
+    void* arr;
+};
+typedef struct array* Array;
+Array parseCurrencies(char* fname);
+HT generateHT(Array c);
+Graph generateGraph(Array c);
+int randomAlphabet(void);
